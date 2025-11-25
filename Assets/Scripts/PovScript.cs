23f46bf2e8f32d4170bc1b63179165d58ea1d143
miacuesta <49257077@est.ort.edu.ar>
2025-11-25 14:33:39 -0300

    esta medio bug el pov

---
+++ Assets/Scripts/PovScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PovScript : MonoBehaviour
+{
+    private float x;
+    private float y;
+    private float sensitivity=-1f;
+    private Vector3 rotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState=CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y = Input.GetAxis("Mouse Y");
+        x = Input.GetAxis("Mouse X");
+        rotate = new Vector3(x, y * sensitivity, 0);
+        transform.eulerAngles = transform.eulerAngles - rotate;
+    }
+}
